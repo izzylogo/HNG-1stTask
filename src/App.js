38f504a59a065c2c1a45ca8754700d705664e436
Israel Omitiran <omitiranisrael@gmail.com>
2022-10-29 19:50:56 +0100

    adjusting the links

--- src/App.js
+++ src/App.js
@@ -14,17 +14,18 @@ function App() {
         <div className="profile">
           <img src={profile_img} id="profile__img" alt="" /> 
           <p className="twit_user" id='twitter'>Annette Black</p> 
+          <p id="slack" style={{display: 'none'}}>izZyDev</p>
           <p></p>
         </div>
         <img src={share_btn} alt="" className="share_btn big" />
         <img src={smal_share} alt="" className="share_btn small" />
         <div className="links">
           <a className='link' id='twitter' target='_blank' rel='noreferrer' href='https://twitter.com/IzzyLogo'>Twitter Link</a>
-          <a className='link' id='btn_zuri' target='_blank' rel='noreferrer' href='https://training.zuri.team/'>Zuri Team</a>
+          <a className='link' id='btn__zuri' target='_blank' rel='noreferrer' href='https://training.zuri.team/'>Zuri Team</a>
           <a className='link' id='books' target='_blank' rel='noreferrer' href='https://books.zuri.team/'>Zuri Books</a>
-          <a className='link' id='book_python' target='_blank' rel='noreferrer' href='https://books.zuri.team/python-for-beginners?ref_id=israel-omitiran'>Python Books</a>
+          <a className='link' id='book__python' target='_blank' rel='noreferrer' href='https://books.zuri.team/python-for-beginners?ref_id=<IzZyDev>'>Python Books</a>
           <a className='link' id='pitch' target='_blank' rel='noreferrer' href='https://background.zuri.team'>Background Check for Coders</a>
-          <a className='link' id='book_design' target='_blank' rel='noreferrer' href='https://books.zuri.team/design-rules'>Design</a>
+          <a className='link' id='book__design' target='_blank' rel='noreferrer' href='https://books.zuri.team/design-rules'>Design</a>
           <div className="icons">
             <img src={slack} alt="" />
             <img src={git} alt="" />
